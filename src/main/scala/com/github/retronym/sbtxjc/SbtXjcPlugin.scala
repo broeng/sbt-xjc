870c39869fa28aa1832789754748f67b4274dcd8
Simon Broeng Jensen <sbj@cwconsult.dk>
2017-12-12 12:16:35 +0100

    Only replace existing source files if content has changed

--- src/main/scala/com/github/retronym/sbtxjc/SbtXjcPlugin.scala
+++ src/main/scala/com/github/retronym/sbtxjc/SbtXjcPlugin.scala
@@ -1,9 +1,14 @@
 package com.github.retronym.sbtxjc
 
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.StandardOpenOption
 
+import sbt.Keys._
 import sbt._
-import Keys._
+
+import scala.io.Codec
+import scala.io.Source
 
 /**
  * Compile Xml Schemata with JAXB XJC.
@@ -76,6 +81,9 @@ object SbtXjcPlugin extends AutoPlugin {
     import streams.log
     def generated = (sourceManaged ** "*.java").get
 
+    val compileOutputTmp =
+      new File(sourceManaged.getParentFile, "xjc_tmp")
+
     val shouldProcess = (xjcSources, generated) match {
       case (Seq(), _)  => false
       case (_, Seq())  => true
@@ -96,24 +104,58 @@ object SbtXjcPlugin extends AutoPlugin {
         case Seq() => Seq()
         case js    => Seq("-extension", "-classpath", js.mkString(pathSeparator))
       }
-      val appOptions = pluginCpOptions ++ Seq("-d", sourceManaged.getAbsolutePath)
+      val appOptions = pluginCpOptions ++ Seq("-d", compileOutputTmp.getAbsolutePath)
       val mainClass  = "com.sun.tools.xjc.XJCFacade"
       val bindings = xjcBindings.map(List("-b",_)).flatten
       jvmCpOptions ++ xjcJvmOpts ++ List(mainClass) ++ appOptions ++ extraCommandLine ++ xsdSourcePaths ++ bindings
     }
 
     if (shouldProcess) {
+      compileOutputTmp.mkdirs()
       sourceManaged.mkdirs()
       log.info("Compiling %d XSD file(s) to %s".format(xjcSources.size, sourceManaged.getAbsolutePath))
       log.debug("XJC java command line: " + options.mkString("\n"))
       val returnCode = Forker(javaHome, options, log)
       if (returnCode != 0) sys.error("Non zero return code from xjc [%d]".format(returnCode))
+      // Copy any changed files
+      (compileOutputTmp ** "*.java").get.foreach { sourceFile =>
+        copyIfChanged(sourceManaged, sourceFile, compileOutputTmp, log.info(_))
+      }
     } else {
       log.debug("No sources newer than products, skipping.")
     }
 
     generated
   }
+
+  private def copyIfChanged(sourceManaged: File, sourceFile: File, sourceFileRoot: File, log: String => Unit): Unit = {
+    val relativeFilePath = sourceFile.getAbsolutePath.substring(sourceFileRoot.getAbsolutePath.length)
+    val destinationFile = new File(sourceManaged, relativeFilePath)
+    // Ensure destination folders exist
+    destinationFile.getParentFile.mkdirs()
+    // Copy the file if it exists
+    readFile(sourceFile) foreach { generatedContent: String =>
+      // ... and has updated content
+      if (!readFile(destinationFile).exists(_ == generatedContent)) {
+        log(s"Copying ${sourceFile.getAbsolutePath} -> ${destinationFile.getAbsolutePath}")
+        Files.write(
+          destinationFile.toPath,
+          Files.readAllBytes(sourceFile.toPath),
+          StandardOpenOption.WRITE,
+          StandardOpenOption.CREATE,
+          StandardOpenOption.TRUNCATE_EXISTING)
+      }
+      // Remove the source file
+      sourceFile.delete()
+    }
+  }
+
+  private def readFile(file: File): Option[String] =
+    if (!file.exists()) {
+      None
+    } else {
+      Some(Source.fromFile(file, Codec.UTF8.name).mkString)
+    }
 
   private def xjcClean(sourceManaged: File, streams: TaskStreams) {
     import streams.log
